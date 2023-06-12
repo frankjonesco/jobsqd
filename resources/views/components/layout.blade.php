d1d9839a85fc81b5800223c88b3e4185459b2dde
“Frank <frank@frankjones.co>
2023-06-12 08:25:29 +0100

    Google Font ROBOTO installed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Job Sqaud</title>
     <meta name="description" content="Find assistance to work abroad within Europe, we're here to guide you through your life changing experience">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,700;0,900;1,400&display=swap" rel="preload" as="stylesheet" onload="this.rel='stylesheet'">
+
     @vite('resources/css/app.css')
 </head>
 <body>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,44 @@
+<nav id="topNav">
+    <div class="container flex justify-between">
+        <a href="/">
+            <img src="{{asset('images/job-squad-logo.png')}}" />
+        </a>
+        <ul>
+            <li>
+                <a href="/">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    CV Builder
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    Gap Year
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    Blog
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    About Us
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    Contact Us
+                </a>
+            </li>
+            <li>
+                <a href="/">
+                    English
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
